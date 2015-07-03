a6c2318f87fafc5ad7fa659f6863f3267c6608d3
Nitesh Mishra <niteshmishra@niteshmishra.in>
2015-07-04 03:45:22 +0530

    chore(karma): included additional files in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,10 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-          'src/**/*.js',
-          'test/**/*.spec.js'
+            'bower_components\angular\angular.min.js',
+            'bower_components\angular-mocks\angular-mocks.js',
+            'src/**/*.js',
+            'test/**/*.spec.js'
         ],
 
 
