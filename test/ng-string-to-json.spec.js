964310575366d0e996ac0dba958dd5f5ac5c4ceb
Nitesh Mishra <niteshmishra@niteshmishra.in>
2015-07-04 18:03:09 +0530

    test(ngStringToJson): added initial structure of test specs

---
+++ test/ng-string-to-json.spec.js
@@ -0,0 +1,13 @@
+describe('ng-string-to-json', function () {
+    var scope, compile;
+
+    beforeEach(module('ngStringToJson'));
+    beforeEach(inject(function (_$rootScope_, _$compile_) {
+        scope = _$rootScope_;
+        compile = _$compile_;
+    }));
+
+    it('should generate proper json', function () {
+
+    });
+});
